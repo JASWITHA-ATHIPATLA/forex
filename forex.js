81ea2ff73e98c2b8116789c89f47b11fef018f02
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2025-11-14 23:04:09 +0530

    Forex

--- forex.js
+++ forex.js
@@ -1,4 +1,4 @@
-const url="https://2024-03-06.currency-api.pages.dev/v1/currencies/eur.json";
+const base_url="https://2024-03-06.currency-api.pages.dev/v1/currencies";
 const dropdown=document.querySelectorAll(".dropdown select");
 for(select of dropdown){
   for(code in countryList){
@@ -13,7 +13,8 @@ let country1=document.querySelector(".country1");
 let country2=document.querySelector(".country2");
 let flagImg1=document.querySelector(".flagImg1");
 let flagImg2=document.querySelector(".flagImg2");
-country1.addEventListener("change", () => {
+let amount=document.querySelector(".amt");
+country1.addEventListener("input", () => {
    const code = countryList[country1.value];
    flagImg1.src = `https://flagsapi.com/${code}/flat/64.png`;
 });
@@ -22,3 +23,15 @@ country2.addEventListener("change", () => {
    const code = countryList[country2.value];
    flagImg2.src = `https://flagsapi.com/${code}/shiny/64.png`;
 });
+let currencyBtn=document.querySelector(".Exchange-currencyBtn");
+currencyBtn.addEventListener("click",(event)=>{
+   event.preventDefault();
+   if(amount.value==="" || amount.value<1){
+      amount.value="1";
+   }
+   const url=`${base_url}/${country1.value.toLowerCase()}/${country2.value.toUpperCase()}.json`;
+   let response=fetch()
+   let message=document.querySelector(".message");
+   let multpy=(amount.value)*
+   //message.innerHTML=`${}`
+})
