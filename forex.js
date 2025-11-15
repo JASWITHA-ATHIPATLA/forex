73eeb56e38c25c7e53591bc6d87bb18a82015173
ATHIPATLA-JASWITHA <jaswitha.23BCB7267@vitapstudent.ac.in>
2025-11-15 11:45:56 +0530

    forex currency converter

--- forex.js
+++ forex.js
@@ -23,15 +23,23 @@ country2.addEventListener("change", () => {
    const code = countryList[country2.value];
    flagImg2.src = `https://flagsapi.com/${code}/shiny/64.png`;
 });
+
 let currencyBtn=document.querySelector(".Exchange-currencyBtn");
-currencyBtn.addEventListener("click",(event)=>{
+currencyBtn.addEventListener("click",async(event)=>{
    event.preventDefault();
    if(amount.value==="" || amount.value<1){
       amount.value="1";
    }
-   const url=`${base_url}/${country1.value.toLowerCase()}/${country2.value.toUpperCase()}.json`;
-   let response=fetch()
-   let message=document.querySelector(".message");
-   let multpy=(amount.value)*
-   //message.innerHTML=`${}`
+   const from = country1.value.toLowerCase();  // e.g. "usd"
+   const to   = country2.value.toLowerCase();
+   const url=`${base_url}/${from}.json`;
+   //console.log(country1.value,country2.value.toLowerCase());
+   let response= await fetch(url);
+   let data= await response.json();
+   //console.log(data);
+   let rate=data[from][to];
+   let result=Number(amount.value)*rate;
+   console.log(result);
+   let message = document.querySelector(".message");
+    message.innerText = `${amount.value} ${country1.value} = ${result.toFixed(2)} ${country2.value}`;
 })
